f8fcc1a2babb632cbfe1b2f16c82a42321315d21
otr1v <100768041+otr1v@users.noreply.github.com>
2023-02-16 19:22:38 +0300

    Update listfunc.h

--- listfunc.h
+++ listfunc.h
@@ -6,28 +6,36 @@ void StartInit(QUEUE* queue)
     queue->tail     = 0;
     queue->capacity = START_CAPACITY;
     queue->size     = 0;
-
     *(CANARY_TYPE *)(queue->data)                         = LEFT_DATA_CANARY;
-       // printf("st%d", queue->data);
+    //    // printf("st%d", queue->data);
     queue->data = queue->data + sizeof(CANARY_TYPE) / sizeof(elem_t);
     *(CANARY_TYPE *)(queue->data + START_CAPACITY)       = RIGHT_DATA_CANARY;
     queue->RIGHT_MASTER = RIGHT_CANARY;
     queue->LEFT_MASTER  = LEFT_CANARY;
-    memset(queue->data, POISON, queue->capacity);
-    memset(queue->next + 1, FREE_CELL, queue->capacity - 1);
+
+    queue->data[0] = 0;
+    queue->next[0] = 0;
+    queue->prev[0] = 0;
+    for (int i = 1; i < START_CAPACITY; i++)
+    {
+        queue->data[i] = POISON;
+        queue->next[i] = FREE_CELL;
+        queue->prev[i] = FREE_CELL;
+    }
+    queue->prev[1] = 0;
 }
 
 void CreateList(QUEUE* queue)
 {
     
     queue->data = (elem_t *) calloc(START_CAPACITY * sizeof(elem_t) + 2 * sizeof(CANARY_TYPE) + 0 , sizeof(char));
-    CHECK_ERR(queue->data = NULL, "NOT ENOUGH MEMORY");
+    CHECK_ERR(queue->data == NULL, "NOT ENOUGH MEMORY");
 
-    queue->next = (int *)    calloc(START_CAPACITY, sizeof(elem_t));
-    CHECK_ERR(queue->data = NULL, "NOT ENOUGH MEMORY");
+    queue->next = (int *)    calloc(START_CAPACITY * sizeof(int), sizeof(char));
+    CHECK_ERR(queue->next == NULL, "NOT ENOUGH MEMORY");
 
-    queue->prev = (int *)    calloc(START_CAPACITY, sizeof(elem_t));
-    CHECK_ERR(queue->data = NULL, "NOT ENOUGH MEMORY");
+    queue->prev = (int *)    calloc(START_CAPACITY, sizeof(int));
+    CHECK_ERR(queue->prev == NULL, "NOT ENOUGH MEMORY");
 
     StartInit(queue);
     
@@ -35,7 +43,7 @@ void CreateList(QUEUE* queue)
 
 //==========================================================
 
-int PushElem(QUEUE* queue, int* element)
+int ListInsert(QUEUE* queue, int* element, int index)           //index - before our number
 {
     if (queue->size >= queue->capacity)
     {
@@ -44,26 +52,47 @@ int PushElem(QUEUE* queue, int* element)
     }
     //linear search
     // queue->data[queue->size] = *element;
-    int freecell = 0;
-    for (int i = 1; i < queue->capacity; i++)
+    if (queue->size == 0)
     {
-        if (queue->next[i] == FREE_CELL)
+        queue->data[1] = *element;
+        queue->prev[1] = 0;
+        queue->size++;
+        queue->head = 1;
+        queue->tail = 1;
+    }
+    else
+    {
+        int freecell = 0;
+        for (int i = 1; i < queue->capacity; i++)
         {
-            freecell = queue->next[i - 1];
-            break;
+            if (queue->next[i] == FREE_CELL)
+            {
+                freecell = i + 1;
+                //printf("%d", queue->next[i - 1]);
+                break;
+            }
+        }
+        if (freecell != 0 )
+        {
+            if (queue->size + 1 != index)
+            {
+
+            }
+            queue->data[freecell]     = *element;
+            queue->next[freecell - 1] = index;
+            queue->next[index - 1]    = freecell;
+            queue->prev[index + 1]    = freecell;
+            queue->prev[freecell + 1] = index;
+        }
+        else 
+        {
+            printf("queue is full");
+            return 0;
         }
     }
-    if (freecell != 0 )
-    {
-        queue->data[freecell] = *element;
-    }
-    else 
-    {
-        printf("queue is full");
-        return 0;
-    }
-    queue->size += 1;
-    queue->tail += 1;
+   
+    // queue->size += 1;
+    // queue->tail += 1;
 
     if (queue->tail == queue->capacity)
     {
@@ -131,5 +160,18 @@ int Listerror(QUEUE* queue)
 
 void ListDump(QUEUE* queue, FILE* dumpfile)
 {
+    fprintf(dumpfile, "digraph structs{\n");
+    // fprintf(dumpfile, "0 [shape=box, style = filled, color = blue, peripheries = 8]");
+    // fprintf(dumpfile, "%d -> %d\n", queue->data[1], queue->data[2]);
+    // fprintf(dumpfile, "0 -> 3338\n1 ->3338}");
+    fprintf(dumpfile, "struct1 [shape = record, color = grey, style = filled, label =\"{");
+    for (int i = 0; i < queue->capacity; i++)
+    {
+        fprintf(dumpfile, " %d.|%d}|{", i, queue->data[i]);
+    }
+    fprintf(dumpfile, "}\"]\n");
 
+
+    fprintf(dumpfile, "}");
+    
 }
