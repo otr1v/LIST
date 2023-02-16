a330dba546fdc6f5359514695c09cb654ecf306d
otr1v <100768041+otr1v@users.noreply.github.com>
2023-02-16 19:23:27 +0300

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -5,6 +5,10 @@ int main()
     QUEUE list = {};
     CreateList(&list);
     int a = 0;
-    AddElem(&list, a);
+    ListInsert(&list, &a, 2);
+    ListInsert(&list, &a, 3);
+
+    FILE* dumpfile = fopen("dumpfile.txt", "w");
+    ListDump(&list, dumpfile);
     return 0;
 }
